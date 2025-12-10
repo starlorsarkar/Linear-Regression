56402d08c61b5633c359ccc0d1639563f2e5e183
Sarkar <5215221@lowes.com>
2025-12-10 12:53:05 +0530

    added logistic regression class to test

--- linear_regression.py
+++ linear_regression.py
@@ -4,3 +4,9 @@ class LinearRegression():
         self.m = m  # slope
         self.b = b  # y-intercept
     pass 
+
+class LogisticRegression():
+    def __init__(self, weights, bias):
+        self.weights = weights  # coefficients for features
+        self.bias = bias        # intercept
+    pass
